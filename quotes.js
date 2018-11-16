73de29908b706663fc4f2807c493196ebe0cf348
Ryan Brennan <ryanbrennan@ryans-mbp.gateway.sonic.net>
2018-11-15 19:41:16 -0800

    Created seed file, sample reviews/quotes

---
+++ quotes.js
@@ -0,0 +1,14 @@
+module.exports.quotes = [
+  "Beautiful place",
+  "One of the most beautiful views.",
+  "Incredible wild trail and one of the best parts of San Francisco.",
+  "Beautiful views! Caught a gorgeous sunset towards the end of the hike. Includes some challenging stairs and climbing that could be an issue for some. Trail was a little crowded and loud at times, but the views were so worth it.",
+  "easy hike with great views!",
+  "Awesome views of the ocean and golden gate. Downside is how much foot traffic there is.",
+  "Fun and easy hike. Gets crowded on Saturday especially when the weather is nice. Will do it again since it’s just minutes away from my home.",
+  "So fortunate to have caught it on a clear day. Easily one of the most beautiful urban hikes I have seen. Parking was a breeze at the visitors center.",
+  "I remember hiking here 40 years ago ... only tiny dirt trails. How time changes. Beautiful nevertheless",
+  "One of my favorite trails in the city and a major reason why I love living here. The views are stunning. Well worth the effort.",
+  "Easy loop, great view, best to go on a weekday though if you can.",
+  "We got to hike the trail at sunset and got some amazing pictures. The view was worth every step!"
+];
---
+++ seed.js
@@ -0,0 +1,3 @@
+const quotes = require('./quotes').quotes;
+
+console.log(quotes);
