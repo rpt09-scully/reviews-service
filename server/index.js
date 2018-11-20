01c4fbac16e935c4c336501811957cda7d110a3a
Ryan Brennan <ryanbrennan@ryans-mbp.gateway.sonic.net>
2018-11-20 11:26:04 -0800

    Changed Port to 3004 -uroot

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const db = require('../database');
 
 const app = express();
-const PORT = 3000;
+const PORT = 3004;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
