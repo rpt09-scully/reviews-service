1f41498fa342fe1d28283d66e0376392dba3267e
Ryan Brennan <ryanbrennan@ryans-mbp.gateway.sonic.net>
2018-11-24 15:39:26 -0800

    cleanup

--- server/index.js
+++ server/index.js
@@ -9,11 +9,11 @@ const PORT = 3004;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-
 //Use Routes
 app.use('/', gets)
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
+
+
