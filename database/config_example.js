b276905cac2a55cae1462c4764f624d4dcd1ee9d
Ryan Brennan <ryanbrennan@ryans-mbp.gateway.sonic.net>
2018-12-18 17:18:39 -0800

    DB test

--- database/config_example.js
+++ database/config_example.js
@@ -4,7 +4,8 @@ module.exports = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: 'revService'
+  port: process.env.DB_PORT,
+  name: process.env.DB_NAME
 };
 
 
