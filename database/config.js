eae9f9c8b1edc37c0e5af1a350e0a8b9908e1e15
Ryan Brennan <ryanbrennan@ryans-mbp.gateway.sonic.net>
2018-12-20 12:57:43 -0800

    anged to DB_HOST

--- database/config.js
+++ database/config.js
@@ -1,22 +1,12 @@
 require('dotenv').config();
 
-console.log({
-
+if (process.env.NODE_ENV) {
+  module.exports = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     port: process.env.DB_PORT,
     database: process.env.DB_NAME
-
-});
-
-if (process.env.NODE_ENV) {
-  module.exports = {
-    host: process.env.RDS_HOST,
-    user: process.env.RDS_USER,
-    password: process.env.RDS_PASS,
-    port: process.env.RDS_PORT,
-    database: process.env.RDS_NAME
   };
 } else {
   module.exports = {
