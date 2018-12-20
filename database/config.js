11c0332d7831c4fd0ebee976d8892c0cdd219780
Ryan Brennan <ryanbrennan@ryans-mbp.gateway.sonic.net>
2018-12-20 12:45:30 -0800

    null

--- database/config.js
+++ database/config.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
 
+console.log({
+  host: process.env.RDS_HOST,
+  user: process.env.RDS_USER,
+  password: process.env.RDS_PASS,
+  port: process.env.RDS_PORT,
+  database: process.env.RDS_NAME
+});
 
 if (process.env.NODE_ENV) {
   module.exports = {
@@ -8,20 +15,14 @@ if (process.env.NODE_ENV) {
     password: process.env.RDS_PASS,
     port: process.env.RDS_PORT,
     database: process.env.RDS_NAME
-  }
+  };
 } else {
   module.exports = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: 'revService'
-  }
+  };
 }
 
-
 //Stashed
-
-
-
-
-
