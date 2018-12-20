cc7475078d5666004d5dd22546ea5d2e2f6a68fe
Ryan Brennan <ryanbrennan@ryans-mbp.gateway.sonic.net>
2018-12-20 12:54:21 -0800

    more testing

--- database/config.js
+++ database/config.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 
 console.log({
-  host: process.env.RDS_HOST,
-  user: process.env.RDS_USER,
-  password: process.env.RDS_PASS,
-  port: process.env.RDS_PORT,
-  database: process.env.RDS_NAME
+
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME
+
 });
 
 if (process.env.NODE_ENV) {
