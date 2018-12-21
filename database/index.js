00556c121fd7d9593813be83085cf4d6cd96804f
Ryan Brennan <ryanbrennan@ryans-mbp.gateway.sonic.net>
2018-12-20 19:01:50 -0800

    slaying more console.logs

--- database/index.js
+++ database/index.js
@@ -3,7 +3,6 @@ const config = require('./config_example.js');
 const moment = require('moment');
 const con = mysql.createConnection(config);
 
-
 //Gets all reviews
 const getAll = cb => {
   let str = `SELECT * FROM reviews INNER JOIN activities WHERE reviews.act_id = activities.activity_id`;
@@ -33,100 +32,91 @@ const getRanking = cb => {
 };
 
 const getStats = (trailId, cb) => {
-  let str =  `SELECT count(*) from reviews WHERE trail_id = ${trailId}`
+  let str = `SELECT count(*) from reviews WHERE trail_id = ${trailId}`;
   let str2 = `SELECT avg(rating) from reviews where trail_id = ${trailId}`;
 
   con.query(str, (err, total) => {
     con.query(str2, (err, avg) => {
       if (err) throw err;
-      let avg1 =  avg[0]
-      let total1 = total[0]
+      let avg1 = avg[0];
+      let total1 = total[0];
 
-      let avg2 = Math.ceil(avg1[Object.keys(avg1)[0]])
-      let total2 = Math.ceil(total1[Object.keys(total1)[0]])
-      cb([avg2, total2])
-    })
-  })
-}
-
-
+      let avg2 = Math.ceil(avg1[Object.keys(avg1)[0]]);
+      let total2 = Math.ceil(total1[Object.keys(total1)[0]]);
+      cb([avg2, total2]);
+    });
+  });
+};
 
 const getReview = (reviewId, cb) => {
   let str = `SELECT reviews.review_id, reviews.user_id, reviews.trail_id, reviews.description, reviews.rating, reviews.date, activities.body \
-  FROM reviews left join activities on activities.activity_id = reviews.act_id WHERE reviews.review_id = ${reviewId}`
+  FROM reviews left join activities on activities.activity_id = reviews.act_id WHERE reviews.review_id = ${reviewId}`;
 
   con.query(str, (err, review) => {
     if (err) throw err;
-    cb(review[0])
-  })
-}
+    cb(review[0]);
+  });
+};
 
 const dateSort = (trailId, sortBy, cb) => {
   let str = `SELECT reviews.review_id, reviews.user_id, reviews.trail_id, reviews.description, reviews.rating, reviews.date, activities.body \
   FROM reviews LEFT JOIN activities on activities.activity_id = reviews.act_id where reviews.trail_id = ${trailId} \
-  ORDER BY str_to_date(date, '%m/%d/%Y') ${sortBy}`
+  ORDER BY str_to_date(date, '%m/%d/%Y') ${sortBy}`;
 
   con.query(str, (err, reviews) => {
     if (err) throw err;
-    cb(reviews)
-  })
-}
+    cb(reviews);
+  });
+};
 
 const ratedSort = (trailId, sortBy, cb) => {
   let str = `SELECT reviews.review_id, reviews.user_id, reviews.trail_id, reviews.description, reviews.rating, reviews.date, activities.body \
   FROM reviews LEFT JOIN activities on activities.activity_id = reviews.act_id where reviews.trail_id = ${trailId} \
-  ORDER by rating ${sortBy}`
+  ORDER by rating ${sortBy}`;
 
   con.query(str, (err, reviews) => {
     if (err) throw err;
-    cb(reviews)
-  })
-}
+    cb(reviews);
+  });
+};
 const jsonFormat = (id, review) => {
-
   let obj = {};
   obj.data = {};
-  obj.data.type = 'reviews'
+  obj.data.type = 'reviews';
   obj.data.id = null;
   obj.data.attributes = {};
 
-    obj.data.id = review.review_id;
-    obj.data.attributes.user_id = review.user_id;
-    obj.data.attributes.trail_id = review.trail_id;
-    obj.data.attributes.body = review.description;
-    obj.data.attributes.activity = review.body;
-    obj.data.attributes.rating = review.rating;
-    obj.data.attributes.date = review.date;
-    return obj;
-}
+  obj.data.id = review.review_id;
+  obj.data.attributes.user_id = review.user_id;
+  obj.data.attributes.trail_id = review.trail_id;
+  obj.data.attributes.body = review.description;
+  obj.data.attributes.activity = review.body;
+  obj.data.attributes.rating = review.rating;
+  obj.data.attributes.date = review.date;
+  return obj;
+};
 
 const dateFormat = (date, cb) => {
   let arr = '1,2,3,4,5,6,7,8,9'.split(',');
-  let years = ['2016', '2017', '2018']
+  let years = ['2016', '2017', '2018'];
   if (date.length === 7 && arr.indexOf(date[0] > -1)) {
     date = '0' + date;
     let timeAgo = moment(date, 'MM/DD/YYYY').fromNow();
-    cb(timeAgo)
-  } else if (date.length !== 8 && years.indexOf(date.slice(date.length - 4)) > -1) {
-    date = '0' + date[0] + '0' + date[1] + date.slice(date.length - 4)
+    cb(timeAgo);
+  } else if (
+    date.length !== 8 &&
+    years.indexOf(date.slice(date.length - 4)) > -1
+  ) {
+    date = '0' + date[0] + '0' + date[1] + date.slice(date.length - 4);
 
     let timeAgo = moment(date, 'MM/DD/YYYY').fromNow();
-    cb(timeAgo)
+    cb(timeAgo);
   } else {
     let timeAgo = moment(date, 'MM/DD/YYYY').fromNow();
-    cb(timeAgo)
+    cb(timeAgo);
   }
 };
-
-
-
-
-
-dateFormat('122016', (time) => {
-  console.log(time)
-})
-
-
+//console log clean
 module.exports = {
   getAll,
   getRanking,
