efe758281d4d01f6166da5b99d78fe17c9bc3360
Ryan Brennan <ryanbrennanbc1@gmail.com>
2019-02-12 15:03:50 -0800

    [wip] sdc

---
+++ database/writeCsv.js
@@ -0,0 +1,49 @@
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const quotes = require('../quotes.js');
+const date = require('../seed.js')
+
+
+
+
+let start = new Date();
+const main = (async () => {
+  const tenMillion = 40000000;
+
+  const csvWriter = createCsvWriter({
+    path: './milly.csv',
+    header: [
+        {id: 'review_id', title: 'review_id'},
+        {id: 'user_id', title: 'user_id'},
+        {id: 'trail_id', title: 'trail_id'},
+        {id: 'description', title: 'description'},
+        {id: 'rating', title: 'rating'},
+        {id: 'date', title: 'date'},
+        {id: 'act_id', title: 'act_id'},
+
+    ]
+  });
+
+
+  csvWriter.writeRecords([])
+
+  const records = [];
+  for (let i = 1; i < tenMillion + 1; i++) {
+    if (records.length > 99 || i === tenMillion) {
+      await csvWriter.writeRecords(records);
+      records.length = 0;
+    }
+    records.push({
+      review_id: i,
+      user_id: (Math.floor(Math.random() * (10000000 - 1) + 1)),
+      trail_id: (Math.floor(Math.random() * (10000000 - 1) + 1)),
+      description: quotes[Math.floor(Math.random() * quotes.length)],
+      rating: (Math.floor(Math.random() * (6 - 1) + 1)),
+      date: date.dateGen(),
+      act_id: (Math.floor(Math.random() * (16 - 1) + 1))
+
+    });
+  }
+
+})();
+
+
